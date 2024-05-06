a22bf6a6a2c2cfbe126bfdfd5e015be930e2a60c
okuzawats <okuzawats@gmail.com>
2024-05-07 05:42:05 +0900

    show NoteView as root element

--- fokusable/fokusableApp.swift
+++ fokusable/fokusableApp.swift
@@ -1,32 +1,17 @@
-//
-//  fokusableApp.swift
-//  fokusable
-//
-//  Created by Toshiki Okuzawa on 2024/05/06.
-//
-
+import ComposableArchitecture
 import SwiftUI
-import SwiftData
 
 @main
 struct fokusableApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+  static let store = Store(initialState: NoteFeature.State()) {
+    NoteFeature()
+  }
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+  var body: some Scene {
+    WindowGroup {
+      NoteView(
+        store: fokusableApp.store
+      )
     }
+  }
 }
