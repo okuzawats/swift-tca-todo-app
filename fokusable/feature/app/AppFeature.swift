8ee660ae83b1c0d6c1df4aadc77dde815a840b7c
okuzawats <okuzawats@gmail.com>
2024-05-10 05:57:13 +0900

    implement AppFeature

---
+++ fokusable/feature/app/AppFeature.swift
@@ -0,0 +1,30 @@
+import ComposableArchitecture
+
+@Reducer
+struct AppFeature {
+  struct State: Equatable {
+    var day: DayFeature.State = DayFeature.State()
+    var note: NoteFeature.State = NoteFeature.State(
+      noteState: NoteState(text: "")
+    )
+  }
+  
+  enum Action {
+    case day(DayFeature.Action)
+    case note(NoteFeature.Action)
+  }
+
+  var body: some ReducerOf<Self> {
+    Scope(state: \.day, action: \.day) {
+      DayFeature()
+    }
+
+    Scope(state: \.note, action: \.note) {
+      NoteFeature()
+    }
+
+    Reduce { state, action in
+      return .none
+    }
+  }
+}
