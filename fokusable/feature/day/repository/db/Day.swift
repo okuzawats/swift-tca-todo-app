49dfd27e48688ff711da9ec059343615c30e11e0
okuzawats <okuzawats@gmail.com>
2024-05-19 06:15:14 +0900

    add date to Day db model

--- fokusable/feature/day/repository/db/Day.swift
+++ fokusable/feature/day/repository/db/Day.swift
@@ -3,10 +3,16 @@ import SwiftData
 
 @Model
 final class Day {
-  init(id: UUID) {
+  init(id: UUID, date: String) {
     self.id = id
+    self.date = date
   }
 
   @Attribute(.unique)
   let id: UUID
+
+  /// a string that represent date.
+  /// string format must be `2024-05-19`.
+  @Attribute(.unique)
+  let date: String
 }
