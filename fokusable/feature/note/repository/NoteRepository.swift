40878eaa671f66d65865c1e63fa3d55ff17df112
okuzawats <okuzawats@gmail.com>
2024-05-22 06:05:33 +0900

    implement fetch note

--- fokusable/feature/note/repository/NoteRepository.swift
+++ fokusable/feature/note/repository/NoteRepository.swift
@@ -3,22 +3,48 @@ import Foundation
 import SwiftData
 
 enum NoteRepositoryError: Error {
+  case fetchError
+  case noEntityError
   case insertionError
 }
 
 struct NoteRepository {
+  var fetch: (UUID) async -> Result<Note, NoteRepositoryError>
   var save: (Int) async -> Result<String, NoteRepositoryError>
 }
 
 extension NoteRepository: DependencyKey {
   static let liveValue: NoteRepository = Self(
+    fetch: { id in
+      @Dependency(\.noteDatabase.context)
+      var context: ModelContext
+      let fetchDispatcher = FetchDescriptor<Note>(
+        predicate: #Predicate {
+          $0.id == id
+        }
+      )
+      let allNote: [Note]
+      do {
+        allNote = try context.fetch(fetchDispatcher)
+      } catch {
+        return .failure(.fetchError)
+      }
+
+      // TODO transform Note to non-db-dependent type
+      if (allNote.isEmpty) {
+        return .failure(.noEntityError)
+      } else {
+        let note = allNote[0]
+        return .success(note)
+      }
+    },
     save: { _ in
       @Dependency(\.noteDatabase.context)
       var context: ModelContext
 
       let inserted: Note
       do {
-        context.insert(Note(id: UUID()))
+        context.insert(Note(id: UUID(), text: "foo"))
         try context.save()
       } catch {
         return .failure(.insertionError)
--- fokusable/feature/note/repository/db/Note.swift
+++ fokusable/feature/note/repository/db/Note.swift
@@ -3,10 +3,13 @@ import SwiftData
 
 @Model
 final class Note {
-  init(id: UUID) {
+  init(id: UUID, text: String) {
     self.id = id
+    self.text = text
   }
 
   @Attribute(.unique)
   let id: UUID
+
+  let text: String
 }
