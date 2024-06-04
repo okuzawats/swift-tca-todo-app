dedb60f0dabd51680d0a6c66d20f8b8061eb5445
okuzawats <okuzawats@gmail.com>
2024-06-05 06:16:55 +0900

    integrate views to FokusableView

--- fokusable/FokusableView.swift
+++ fokusable/FokusableView.swift
@@ -4,17 +4,31 @@ import SwiftUI
 struct FokusableView: View {
   @Bindable
   var store: StoreOf<FokusableFeature>
-
+  
   var body: some View {
     NavigationSplitView {
-      DayView(
-        store: store.scope(state: \.day, action: \.day)
-      )
+      List {
+        //        ForEach(store.days) { item in
+        //          Text("\(item.day)")
+        //            .onTapGesture {
+        //              store.send(.onDaySelected(item.id))
+        //            }
+        //        }
+      }.onAppear {
+        //        store.send(.onEnter)
+      }
       .navigationSplitViewColumnWidth(min: 180, ideal: 200)
     } detail: {
-      NoteView(
-        store: store.scope(state: \.note, action: \.note)
-      )
+      List {
+        //        ForEach(store.items) { item in
+        //          Text(item.toPresentation())
+        //            .padding(.bottom, 4)
+        //        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+      .onAppear {
+        //        store.send(.onEntered(UUID())) // TODO fix UUID
+      }
     }
   }
 }
