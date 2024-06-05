dff9b650aa1c878e1c32a9557c86e832e56bc1c4
okuzawats <okuzawats@gmail.com>
2024-06-06 05:39:45 +0900

    rename onDaySelected to onSelectedDay

--- fokusable/FokusableFeature.swift
+++ fokusable/FokusableFeature.swift
@@ -13,7 +13,7 @@ struct FokusableFeature {
   enum Action {
     case onEnter
     case onFetchedDays([Day])
-    case onDaySelected(DayItem)
+    case onSelectedDay(DayItem)
   }
 
   @Dependency(\.dayRepository) var dayRepository: DayRepository
@@ -58,7 +58,7 @@ struct FokusableFeature {
         )
         return .none
 
-      case let .onDaySelected(day: DayItem):
+      case let .onSelectedDay(day: DayItem):
         return .none
       }
     }
