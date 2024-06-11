d56942936d20d5553c4364e0897526feacc012d6
okuzawats <okuzawats@gmail.com>
2024-06-12 06:26:03 +0900

    define Comparable as extension

--- fokusable/day/DayItem.swift
+++ fokusable/day/DayItem.swift
@@ -1,9 +1,23 @@
 import Foundation
 
-struct DayItem: Equatable, Comparable, Identifiable {
+struct DayItem: Equatable, Identifiable {
   let id: UUID
   let day: String
 
+  init(id: UUID, day: String) {
+    // YYYY-MM-DD
+    let pattern: String = #"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$"#
+    let regex: NSRegularExpression = try! NSRegularExpression(pattern: pattern, options: [])
+    let range: NSRange = NSRange(location: 0, length: day.utf16.count)
+    let match = regex.firstMatch(in: day, range: range)
+    precondition(match != nil)
+    
+    self.id = id
+    self.day = day
+  }
+}
+
+extension DayItem: Comparable {
   static func < (lhs: DayItem, rhs: DayItem) -> Bool {
     let lhsDate = lhs.day.components(separatedBy: "-")
     let rhsDate = rhs.day.components(separatedBy: "-")
@@ -18,17 +32,5 @@ struct DayItem: Equatable, Comparable, Identifiable {
       return true
     }
     return false
-  }
-
-  init(id: UUID, day: String) {
-    // YYYY-MM-DD
-    let pattern: String = #"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$"#
-    let regex: NSRegularExpression = try! NSRegularExpression(pattern: pattern, options: [])
-    let range: NSRange = NSRange(location: 0, length: day.utf16.count)
-    let match = regex.firstMatch(in: day, range: range)
-    precondition(match != nil)
-    
-    self.id = id
-    self.day = day
   }
 }
