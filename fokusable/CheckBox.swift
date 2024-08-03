5c82cff5c5daf047afd4cbf7666c615a26eaf147
okuzawats <okuzawats@gmail.com>
2024-08-03 16:14:54 +0900

    CheckBoxを作成

---
+++ fokusable/CheckBox.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+/// チェックボックスのコンポーネント
+///
+/// チェックボックスの未チェック状態、チェック状態を切り替えることのできるコンポーネント。
+/// SF Symbolsを用いている。
+struct CheckBox: View {
+  private var isChecked: Bool
+  
+  /// - Parameters:
+  ///   - isChecked: If true, check box is checked.
+  init(isChecked: Bool) {
+    self.isChecked = isChecked
+  }
+  
+  var body: some View {
+    if (isChecked) {
+      Image(systemName: "checkmark.square")
+    } else {
+      Image(systemName: "square")
+    }
+  }
+}
