53d1f9b0f86dd5e55a53772d9f56f4b95b9c32a4
okuzawats <okuzawats@gmail.com>
2024-08-20 21:35:56 +0900

    ドキュメンテーションコメントの追加

--- fokusable/day/DayRepository.swift
+++ fokusable/day/DayRepository.swift
@@ -3,13 +3,19 @@ import Dependencies
 import Foundation
 import SwiftData
 
+/// DayRepositoryで発生し得るエラー型
 enum DayRepositoryError: Error {
+  /// 読み込み失敗を表すエラー型
   case fetchError
+  /// 保存失敗を表すエラー型
   case insertionError
 }
 
+/// 日付集約に対するリポジトリ
 struct DayRepository {
+  /// 全ての日付を取得する。
   var fetchAll: () async -> Result<IdentifiedArrayOf<DayItem>, DayRepositoryError>
+  /// 日付を保存する。
   var save: (DayItem) async -> Result<String, DayRepositoryError>
 }
 
